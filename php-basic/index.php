b33c941f63d9d3e559b22369654322432f1f472c
yunapiyo <vocalolove130919@gmail.com>
2024-09-12 19:17:54 +0900

    php-basicをプッシュ

---
+++ php-basic/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+<body>
+  <p>この文章はHTMLで出力しています。</p>
+  <p>
+    <?php
+    echo 'この文章はPHPで出力しています。';
+    ?>
+  </p>
+</body>
+</html>
